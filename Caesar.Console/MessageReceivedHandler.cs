eb6821459478fb2e4809a658427fc4626964c802
Viktor Prykhidko <viktor.prykhidko@softwarium.net>
2021-08-04 18:49:16 +0300

    Receive updated using polling

---
+++ Caesar.Console/MessageReceivedHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace Caesar.Console
+{
+    public class MessageReceivedHandler: IUpdateHandler
+    {
+        public async Task HandleUpdate(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            if (update.Message is { } message)
+            {
+                await botClient.SendTextMessageAsync(message.Chat, $"You said:{message.Text}" , cancellationToken: cancellationToken);
+            }
+        }
+
+        public async Task HandleError(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            if (exception is ApiRequestException apiRequestException)
+            {
+                await botClient.SendTextMessageAsync(123, apiRequestException.ToString(), cancellationToken: cancellationToken);
+            }
+        }
+
+        public UpdateType[]? AllowedUpdates => null;
+    }
+}
--- Caesar.Console/Program.cs
+++ Caesar.Console/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System.Threading;
-using System.Threading.Tasks;
 using Telegram.Bot;
-using Telegram.Bot.Args;
 
 namespace Caesar.Console
 {
@@ -9,31 +7,18 @@ namespace Caesar.Console
     {
         private static ITelegramBotClient _botClient;
 
-        static async Task Main(string[] args)
+        static void Main(string[] args)
         {
-            _botClient = new TelegramBotClient("{{{token}}}");
+            _botClient = new TelegramBotClient("{{{token}}");
+
             var cts = new CancellationTokenSource();
             var cancellationToken = cts.Token;
+            var messageReceivedHandler = new MessageReceivedHandler();
 
-
-            _botClient.OnMessage += Bot_OnMessage;
-            _botClient.StartReceiving();
+            _botClient.StartReceiving(messageReceivedHandler, cancellationToken);
 
             System.Console.WriteLine("Press any key to exit");
             System.Console.ReadKey();
-
-            _botClient.StopReceiving();
-        }
-
-        static async void Bot_OnMessage(object sender, MessageEventArgs e)
-        {
-            if (e.Message.Text != null)
-            {
-                System.Console.WriteLine($"Received a text message in chat {e.Message.Chat.Id}.");
-
-                await _botClient.SendTextMessageAsync(chatId: e.Message.Chat, text: "You said:\n" + e.Message.Text
-                );
-            }
         }
     }
 }
